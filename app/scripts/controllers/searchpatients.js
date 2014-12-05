75bf8144f13d1480f5037c30cf3e497e949246da
Bradley Davis <bradley.davis@unc.edu>
2014-12-04 20:14:08 -0500

    working ajax tables with correct promises

--- app/scripts/controllers/searchpatients.js
+++ app/scripts/controllers/searchpatients.js
@@ -8,40 +8,42 @@
  * Controller of the frontendMark2App
  */
 angular.module('frontendMark2App')
-  .controller('SearchpatientsCtrl',['$scope', 'Patients', 'ngTableParams', '$filter', function ($scope, Patients, ngTableParams, $filter) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    .controller('SearchpatientsCtrl', function($scope, $filter, $resource, Patients, ngTableParams) {
 
-    var request = Patients.all();
-    $scope.patients = request;
-    $scope.filters = {
-        lastName: '',
-        firstName: ''
-    };
+        // var Patients = $resource('http://localhost:8080/api/patients');
+        Patients.all().$promise.then(function(data) {
 
-    $scope.tableParams = new ngTableParams({
+            $scope.tableParams = new ngTableParams({
                 page: 1,
                 count: 10,
-                filter: $scope.filters,
+                filter: {
+
+                },
                 sorting: {
                     lastName: 'asc'
                 }
-        }, {
-            total: request.length,
-            getData: function ($defer, params) {
-                request.$promise.then(function(data) {
-                        var orderedData = params.sorting() ? $filter('orderBy')(data, params.orderBy()) : data;
-                        var filteredData = params.filter() ? $filter('filter')(data, params.filter()) : orderedData;
+            }, {
+                total: 0,
+                getData: function($defer, params) {
+                    // use build-in angular filter
+                    var filteredData = params.filter() ?
+                        $filter('filter')(data, params.filter()) :
+                        $scope.data;
+                    var orderedData = params.sorting() ?
+                        $filter('orderBy')(filteredData, params.orderBy()) :
+                        $scope.data;
 
-                        $scope.patients = filteredData;//orderedData.slice((params.page() - 1) * params.count(), params.page() *params.count);
-                        // params.total(orderedData.length);
-                        $defer.resolve($scope.patients);
-                });
-            }
+                    params.total(orderedData.length); // set total for recalc pagination
+                    $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+                }
+            });
+
+            $scope.data = data;
+
+        });
+
+        $scope.changeSelection = function(patient) {
+            console.info(patient);
+        };
+
     });
-  }
-  
-]);
--- app/scripts/resources/patients.js
+++ app/scripts/resources/patients.js
@@ -1,8 +1,9 @@
 angular.module('patient', ['ngResource']).factory('Patients', ['$resource', function ($resource) {
    var host = 'http://localhost:8080';
-   return $resource(host + '/api/patients/:patientId', null,
+   return $resource(host + '/api/patients', null,
             {
                 'getPatient': {
+                    url: host + '/api/patients/:patientId',
                     method: 'GET',
                     params: {patientId: '@patientId'}
                 },
